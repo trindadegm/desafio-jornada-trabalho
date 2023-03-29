0a0223c6ba3b4fd3eac7fba86825c76f6c0496ba
Gustavo Moitinho Trindade <gmoitinhotrindade@gmail.com>
2023-03-29 08:13:34 -0300

    Remove the please

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { WorkSchedule } from './views/WorkSchedule.js'
 function App() {
   return (
     <div className="App">
-      <p>Please</p>
       <Grid container justifyContent="center">
         <Grid item xs={12} lg={8}>
           <WorkSchedule/>
